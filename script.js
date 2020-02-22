ad0cc0a08e8af13ab45943702f1404632d7dd97e
OdearOgy <hovhan198@gmail.com>
2020-02-23 02:42:09 +0400

    Fixed js editor

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const jseditor = document.querySelector(`textarea[name='js-input']`);
-
-
-jseditor.addEventListener('change', () => {
-    let jscode = jseditor.value;
-
-    const preview = document.getElementById('out');
-
-    preview.textContent = jscode;
-
-})
+const preview = document.querySelector('.output').contentWindow.document;
+jseditor.addEventListener('change', (e) => {
+    if (preview.querySelector('.rendered-js')) {
+        preview.querySelector('.rendered-js').remove();
+    }
+    const jscode = preview.createElement('script');
+    jscode.classList.add('rendered-js');
+    jscode.appendChild(preview.createTextNode(`${e.target.value}`));
+    preview.body.appendChild(jscode);
+});
