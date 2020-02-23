048a430cf13e3aa82cd0a7fc282d77e2ba5a6b47
OdearOgy <hovhan198@gmail.com>
2020-02-24 00:26:47 +0400

    Refactored code render function

--- script.js
+++ script.js
@@ -1,47 +1,68 @@
 const preview = document.querySelector('iframe.output').contentWindow.document;
 const codeInput = Array.from(document.getElementsByClassName(`code__input`));
 
-
-
 codeInput.forEach((input) => {
     input.addEventListener('change', (e) => {
         const language = e.target.dataset.language;
-        const code = e.target.value
+        const code = e.target.value;
+        let props = {};
+
         switch(language) {
             case 'html':
-                if (preview.querySelector('.rendered-html')) {
-                    preview.querySelector('.rendered-html').remove();
-                }
-                const htmlCode = preview.createElement('div');
-                htmlCode.classList.add('rendered-html');
-                htmlCode.innerHTML = code;
-                try {
-                    preview.body.insertBefore(htmlCode, querySelector('.rendered-js'));
-                } catch(err) {
-                    preview.body.appendChild(htmlCode);
+                props = {
+                    lang: 'html',
+                    elm: 'div',
+                    elmParent: 'body',
+                    doc: preview,
+                    val: code,
                 }
                 break;
             case 'css':
-                if (preview.querySelector('.rendered-css')) {
-                    preview.querySelector('.rendered-css').remove();
+                 props = {
+                    lang: 'css',
+                    elm: 'style',
+                    elmParent: 'head',
+                    doc: preview,
+                    val: code,
                 }
-                const cssCode = preview.createElement('style');
-                cssCode.classList.add('rendered-css');
-                cssCode.appendChild(preview.createTextNode(code));
-                preview.head.appendChild(cssCode);
                 break;
             case 'js':
-                if (preview.querySelector('.rendered-js')) {
-                    preview.querySelector('.rendered-js').remove();
+                props = {
+                    lang: 'js',
+                    elm: 'script',
+                    elmParent: 'body',
+                    doc: preview,
+                    val: code,
                 }
-                const jsCode = preview.createElement('script');
-                jsCode.classList.add('rendered-js');
-                jsCode.appendChild(preview.createTextNode(code));
-                preview.body.appendChild(jsCode);
                 break;
             default:
                 console.log('Language is not defined');
                 break;
         }
+        renderCode(props);
     });
 });
+
+
+function renderCode(props) {
+    const { lang, elm, elmParent, doc, val } = props;
+
+    if (doc.querySelector(`.rendered-${lang}`)) {
+        doc.querySelector(`.rendered-${lang}`).remove();
+    }
+
+    const renderedCode = doc.createElement(`${elm}`);
+    renderedCode.classList.add(`rendered-${lang}`);
+
+    if (lang === 'html') {
+        renderedCode.innerHTML = val;
+        try {
+            doc[elmParent].insertBefore(renderedCode, doc.querySelector('.rendered-js'))
+        } catch(err) {
+            doc[elmParent].appendChild(renderedCode);
+        }
+    } else {
+        renderedCode.appendChild(doc.createTextNode(val))
+        doc[elmParent].appendChild(renderedCode);
+    }
+}
