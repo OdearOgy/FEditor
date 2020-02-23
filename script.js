cbaa0ad578805e3279e53268841e88d7f092810e
OdearOgy <hovhan198@gmail.com>
2020-02-23 21:56:57 +0400

    Added html, css, js editors

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
-const jseditor = document.querySelector(`textarea[name='js-input']`);
-const preview = document.querySelector('.output').contentWindow.document;
-jseditor.addEventListener('change', (e) => {
-    if (preview.querySelector('.rendered-js')) {
-        preview.querySelector('.rendered-js').remove();
-    }
-    const jscode = preview.createElement('script');
-    jscode.classList.add('rendered-js');
-    jscode.appendChild(preview.createTextNode(`${e.target.value}`));
-    preview.body.appendChild(jscode);
+const preview = document.querySelector('iframe.output').contentWindow.document;
+const codeInput = Array.from(document.getElementsByClassName(`code__input`));
+
+
+
+codeInput.forEach((input) => {
+    input.addEventListener('change', (e) => {
+        const language = e.target.dataset.language;
+        const code = e.target.value
+        switch(language) {
+            case 'html':
+                if (preview.querySelector('.rendered-html')) {
+                    preview.querySelector('.rendered-html').remove();
+                }
+                const htmlCode = preview.createElement('div');
+                htmlCode.classList.add('rendered-html');
+                htmlCode.innerHTML = code;
+                try {
+                    preview.body.insertBefore(htmlCode, querySelector('.rendered-js'));
+                } catch(err) {
+                    preview.body.appendChild(htmlCode);
+                }
+                break;
+            case 'css':
+                if (preview.querySelector('.rendered-css')) {
+                    preview.querySelector('.rendered-css').remove();
+                }
+                const cssCode = preview.createElement('style');
+                cssCode.classList.add('rendered-css');
+                cssCode.appendChild(preview.createTextNode(code));
+                preview.head.appendChild(cssCode);
+                break;
+            case 'js':
+                if (preview.querySelector('.rendered-js')) {
+                    preview.querySelector('.rendered-js').remove();
+                }
+                const jsCode = preview.createElement('script');
+                jsCode.classList.add('rendered-js');
+                jsCode.appendChild(preview.createTextNode(code));
+                preview.body.appendChild(jsCode);
+                break;
+            default:
+                console.log('Language is not defined');
+                break;
+        }
+    });
 });
