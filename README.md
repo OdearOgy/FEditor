78b00b81f53c3e4c341bac28563559a439a7ed7a
OdearOgy <hovhan198@gmail.com>
2020-04-18 05:02:31 +0400

    Added readme, fixed editor's margins and position

---
+++ README.md
@@ -0,0 +1,17 @@
+# Front-End Editor
+
+A HTML/CSS/JS editor.
+
+## Motivation
+
+I was curious about how the codepen works and so decided to create something similar, but also simplistic. It wasn't that hard, and so this is going to be an ongoing project.
+
+## TODO
+
+- [ ] Dark/Light mode switcher
+
+- [ ] Syntax highlighting
+
+- [ ] Save code to a file
+
+- [ ] User profiles
--- script.js
+++ script.js
@@ -2,67 +2,66 @@ const preview = document.querySelector('iframe.output').contentWindow.document;
 const codeInput = Array.from(document.getElementsByClassName(`code__input`));
 
 codeInput.forEach((input) => {
-    input.addEventListener('change', (e) => {
-        const language = e.target.dataset.language;
-        const code = e.target.value;
-        let props = {};
+	input.addEventListener('change', (e) => {
+		const language = e.target.dataset.language;
+		const code = e.target.value;
+		let props = {};
 
-        switch(language) {
-            case 'html':
-                props = {
-                    lang: 'html',
-                    elm: 'div',
-                    elmParent: 'body',
-                    doc: preview,
-                    val: code,
-                }
-                break;
-            case 'css':
-                 props = {
-                    lang: 'css',
-                    elm: 'style',
-                    elmParent: 'head',
-                    doc: preview,
-                    val: code,
-                }
-                break;
-            case 'js':
-                props = {
-                    lang: 'js',
-                    elm: 'script',
-                    elmParent: 'body',
-                    doc: preview,
-                    val: code,
-                }
-                break;
-            default:
-                console.log('Language is not defined');
-                break;
-        }
-        renderCode(props);
-    });
+		switch (language) {
+			case 'html':
+				props = {
+					lang: 'html',
+					elm: 'div',
+					elmParent: 'body',
+					doc: preview,
+					val: code,
+				};
+				break;
+			case 'css':
+				props = {
+					lang: 'css',
+					elm: 'style',
+					elmParent: 'head',
+					doc: preview,
+					val: code,
+				};
+				break;
+			case 'js':
+				props = {
+					lang: 'js',
+					elm: 'script',
+					elmParent: 'body',
+					doc: preview,
+					val: code,
+				};
+				break;
+			default:
+				console.log('Language is not defined');
+				break;
+		}
+		renderCode(props);
+	});
 });
 
-
 function renderCode(props) {
-    const { lang, elm, elmParent, doc, val } = props;
+	const { lang, elm, elmParent, doc, val } = props;
 
-    if (doc.querySelector(`.rendered-${lang}`)) {
-        doc.querySelector(`.rendered-${lang}`).remove();
-    }
+	if (doc.querySelector(`.rendered-${lang}`)) {
+		doc.querySelector(`.rendered-${lang}`).remove();
+	}
 
-    const renderedCode = doc.createElement(`${elm}`);
-    renderedCode.classList.add(`rendered-${lang}`);
+	const renderedCode = doc.createElement(`${elm}`);
+	renderedCode.classList.add(`rendered-${lang}`);
 
-    if (lang === 'html') {
-        renderedCode.innerHTML = val;
-        try {
-            doc[elmParent].insertBefore(renderedCode, doc.querySelector('.rendered-js'))
-        } catch(err) {
-            doc[elmParent].appendChild(renderedCode);
-        }
-    } else {
-        renderedCode.appendChild(doc.createTextNode(val))
-        doc[elmParent].appendChild(renderedCode);
-    }
+	if (lang === 'html') {
+		renderedCode.innerHTML = val;
+		try {
+			doc[elmParent].insertBefore(renderedCode, doc.querySelector('.rendered-js'));
+		} catch (err) {
+			doc[elmParent].appendChild(renderedCode);
+		}
+	} else {
+		renderedCode.appendChild(doc.createTextNode(val));
+		doc[elmParent].appendChild(renderedCode);
+	}
 }
